1911117ec7bd4cc5bd1abf80676ab2ef0a3abcdb
Warunyoo <hicombid_1093@hotmail.com>
2018-03-18 21:33:43 +0700

    Create notify.php

---
+++ notify.php
@@ -0,0 +1,20 @@
+  <?php
+  $ch = curl_init();
+  curl_setopt( $ch, CURLOPT_URL, "https://notify-api.line.me/api/notify");
+  curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, 0);
+  curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, 0);
+  curl_setopt( $ch, CURLOPT_POST, 1);
+  curl_setopt( $ch, CURLOPT_POSTFIELDS, "message=$message");
+  curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, 1);
+  $headers = array( "Content-type: application/x-www-form-urlencoded", "Authorization: Bearer $token", );
+  curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+  curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
+  $result = curl_exec( $ch );
+  curl_close( $ch );
+
+  return $result;
+
+$message = 'สวัสดี';
+$token = 'XPGm9h6RxNpW9UzyxuiJmGvG5JIyGDXzd9UymxV0Cwo';
+
+echo send_line_notify($message, $token);
