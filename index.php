8af309b485f450f3349fb470a9c7e3f3c7b60101
Warunyoo <hicombid_1093@hotmail.com>
2018-03-18 20:29:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,3 @@
 <?php
 echo "Hello LINE BOT";
 echo "eye tracking";
-
-define(‘LINE_API’,”https://notify-api.line.me/api/notify");
- 
-$token = “XPGm9h6RxNpW9UzyxuiJmGvG5JIyGDXzd9UymxV0Cwo”;
-$str = “Hello Eye tracking”;
- 
-$res = notify_message($str,$token);
-print_r($res);
-function notify_message($message,$token){
- $queryData = array(‘message’ => $message);
- $queryData = http_build_query($queryData,’’,’&’);
- $headerOptions = array( 
-         ‘http’=>array(
-            ‘method’=>’POST’,
-            ‘header’=> “Content-Type: application/x-www-form-urlencoded\r\n”
-                      .”Authorization: Bearer “.$token.”\r\n”
-                      .”Content-Length: “.strlen($queryData).”\r\n”,
-            ‘content’ => $queryData
-         ),
- );
- $context = stream_context_create($headerOptions);
- $result = file_get_contents(LINE_API,FALSE,$context);
- $res = json_decode($result);
- return $res;
-}
-?>
