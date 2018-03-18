734fade69a46f74049afb4cd76715b44c9fee9fa
Warunyoo <hicombid_1093@hotmail.com>
2018-03-18 21:24:45 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,6 +1,6 @@
 <?php
-$access_token = 'sPT+QnFHq3lfgGW/83UTBrOkkiuuGLN/iDyOThomaLdA4dR0yt7FQYuQ8e0GLM5RbkiXSqcp4LcC+0jUCnWL6Y3Vsf/UB7SRd18t1/3N1p9nWxzMeLJp8GtRTRq7IKjGwE32Y18f6U6EXpt4w3bPDQdB04t89/1O/w1cDnyilFU=
-';
+$access_token = "sPT+QnFHq3lfgGW/83UTBrOkkiuuGLN/iDyOThomaLdA4dR0yt7FQYuQ8e0GLM5RbkiXSqcp4LcC+0jUCnWL6Y3Vsf/UB7SRd18t1
+/3N1p9nWxzMeLJp8GtRTRq7IKjGwE32Y18f6U6EXpt4w3bPDQdB04t89/1O/w1cDnyilFU=";
 $url = 'https://api.line.me/v1/oauth/verify';
 $headers = array('Authorization: Bearer ' . $access_token);
 $ch = curl_init($url);
